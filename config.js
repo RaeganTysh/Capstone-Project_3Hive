1e05bc7b9a029b39b9027915101c4d2b262ebe8d
RaeganTysh <rae-gan@shaw.ca>
2020-11-22 12:32:35 -0700

    Initial Commit

---
+++ config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+    user: 'sa',
+    password: 'BVC12345',
+    server: 'ACERLAPTOP\\SQLEXPRESS',
+    database: 'Eval',
+    
+    port: 1433,
+    options: {
+        encrypt: true,
+        enableArithAbort: true
+    }
+};
+
+//'NELSON\\SQLEXPRESS'
+
+//'DESKTOP-JVT431S\\SQLEXPRESS01', 
+
+//'DANY_PC\\SQLEXPRESS'
+
+//'ACERLAPTOP\\SQLEXPRESS'
+
